6c53a8a739b53dbe6dd592d8f7906cd549d89c8f
Jure <jure.fabjan93@gmail.com>
2019-07-24 13:53:45 +0200

    Bugs in Input class and creation of Model class.

--- Model.py
+++ Model.py
@@ -1,11 +1,16 @@
-import modeller
+import os
+
 import Bio.AlignIO as AlignIO
+import Bio.PDB as PDB
 import Bio.PDB.PDBParser as PDBParser
 import Bio.PDB.Polypeptide as Polypeptide
-import Bio.pairwise2 as pairwise2
+import Bio.SeqUtils as SeqUtils
 import Bio.SubsMat.MatrixInfo as MatrixInfo
-import Bio.Seq
-import Bio.PDB as PDB
+import Bio.pairwise2 as pairwise2
+import modeller
+import modeller.automodel as automodel
+
+modeller.log.verbose()
 
 
 class _Select(PDB.Select):
@@ -13,9 +18,24 @@ class _Select(PDB.Select):
     Class to overwrite the selection class in Bio.PDB. Used for selecting the included chains
     during the cleaning of the modeller input structure.
     """
-    def __init__(self, selected_chains):
+    def __init__(self):
         super(_Select, self).__init__()
-        self.chains = selected_chains
+        self.chains = []
+        self.residues = []
+
+    def extend(self, extension, ext_type):
+        """
+        Extends the list of the selected chains or residues.
+        :param extension: List of the objects to include.
+        :param ext_type: Type of the objects in the list:
+        c == chain
+        r == residue
+        :return: None
+        """
+        if ext_type == "c":
+            self.chains += extension
+        elif ext_type == "r":
+            self.residues += extension
 
     def accept_chain(self, chain):
         """
@@ -24,6 +44,16 @@ class _Select(PDB.Select):
         :return: True if chain is in the selected list, else False
         """
         if chain in self.chains:
+            return True
+        return False
+
+    def accept_residue(self, residue):
+        """
+        Overwritten method for returning the selection.
+        :param residue: Residue to be checked.
+        :return: True if residue iis in the selected list, else False
+        """
+        if residue in self.residues:
             return True
         return False
 
@@ -48,6 +78,7 @@ class Input:
             self.structure_name = ".".join(structure_loc.split("/")[-1].split(".")[:-1])
         self.target = target
         self.target_name = target_name
+        self.input_loc = f"{self.structure_loc}/Model"
 
         self.structure = PDBParser(PERMISSIVE=True).get_structure(self.structure_name,
                                                                   f"{self.structure_loc}/{self.structure_name}.pdb")
@@ -59,29 +90,36 @@ class Input:
         # Extraction of the chain names
         self.chain_names = [(compound["molecule"].split(",")[0].split(" ")[-1].capitalize(),
                              [chain.capitalize() for chain in compound["chain"].split(", ")]) for compound in
-                              self.compounds.values() if "gamma" in compound["molecule"]]
+                            self.compounds.values() if "gamma" in compound["molecule"]]
 
         # Extraction of information about chains
         self.structure_chains = {}
+        self.structure_chains_reference = {}
         self.chains_extract()
+        # Order of chains, used in writing the alignments, to conserve the order
+        # used in the template structure file
+        self.chains_ordered = [ch.id for ch in self.structure.get_chains() if ch.id in self.structure_chains.keys()]
 
         # Building the information about target chains in relation to template chains
         self.target_chains = {}
         self.chains_build()
 
-        # Getting the start and end AA number in the template structure (for now a very brute force approach)
-        for chain in self.structure_chains.keys():
-            aa = list(self.structure[0][chain].get_residues())
-            self.structure_chains[chain]["start"] = aa[0].full_id[-1][1]
-            self.structure_chains[chain]["end"] = aa[-1].full_id[-1][1]
-
-        # Writing of the PIR file; format:
-        self.input_pir_loc = f"{self.structure_loc}/{self.target_name}.pir"
-        self.pir_write()
-
         # Cleaning and preparing the PDB of the template
-        self.input_pdb_loc = f"{self.structure_loc}/{self.structure_name}_cleaned.pdb"
+        # Getting the start and end AA number in the cleaned template structure
+        self.input_pdb_loc = f"{self.input_loc}/{self.structure_name.upper()}.pdb"
         self.pdb_clean()
+
+        # Writing of the PIR/ALI file
+        try:
+            os.mkdir(self.input_loc)
+        except FileExistsError:
+            pass
+
+        self.input_ali_loc = f"{self.input_loc}/{self.target_name}.ali"
+        self.ali_write()
+
+        # self.input_pir_loc = f"{self.input_loc}/{self.target_name}.pir"
+        # self.pir_write()
 
     def chains_extract(self):
         """
@@ -101,6 +139,11 @@ class Input:
                 self.structure_chains[chain] = {"name": sub_name,
                                                 "gaps": [i for i, aa in enumerate(alignment[0][1]) if aa == "-"],
                                                 "sequence": alignment[0][0]}
+
+                self.structure_chains_reference[chain] = {"name": sub_name,
+                                                          "gaps": [i for i,
+                                                                         aa in enumerate(alignment[0][1]) if aa == "-"],
+                                                          "sequence": alignment[0][0]}
 
     def chains_build(self):
         """
@@ -143,7 +186,8 @@ class Input:
             break_sequences = break_sequences[:-1]
 
         # Getting the aligned sequences
-        seq1_out = str([x for x in self.sequences if (seq1_name in x.name and seq2_name[0].lower() == x.name[-2])][0].seq)
+        seq1_out = str([x for x in self.sequences if
+                        (seq1_name in x.name and seq2_name[0].lower() == x.name[-2])][0].seq)
         seq2_out = str([x for x in self.sequences if seq2_name in x.name][0].seq)
         # Removing gaps present in both sequences
         gaps = [i for i, (s1, s2) in enumerate(zip(seq1_out, seq2_out)) if s1 == s2 == "-"]
@@ -178,19 +222,47 @@ class Input:
         """
         # Sorted for possible better viewing of the generated file by humans
         out = []
-        for chain in sorted(self.structure_chains.keys()):
+        for chain in self.chains_ordered:
             out.append("\n".join([f">P1;{self.structure_name}",
                                   f"structure:{self.structure_name}:{self.structure_chains[chain]['start']}:{chain}:{self.structure_chains[chain]['end']}:{chain}:{self.structure_name}:::",
-                                  *[self.structure_chains[chain]["sequence"][i:i+51] for i in range(0, len(self.structure_chains[chain]["sequence"]), 50)],
+                                  *[self.structure_chains[chain]["sequence"][i:i+50] for i in range(0, len(self.structure_chains[chain]["sequence"]), 50)],
                                   "*"]))
 
             out.append("\n".join([f">P1;{self.target_name}",
                                   f"sequence:{self.target_name}:1:{chain}::{chain}:{self.target_name}:::",
-                                  *[self.target_chains[chain]["sequence"][i:i+51] for i in range(0, len(self.target_chains[chain]["sequence"]), 50)],
+                                  *[self.target_chains[chain]["sequence"][i:i+50] for i in range(0, len(self.target_chains[chain]["sequence"]), 50)],
                                   "*"]))
 
         with open(self.input_pir_loc, "w") as file:
             file.write("\n".join(out))
+
+    def ali_write(self):
+        """
+        Writes the ALI file, needed as an input to the modeller.
+        >P1;name
+        structure/sequence:name:beginning_residue:beginning_chain:ending_residue:ending_chain:name:::
+        :return: None
+        """
+        # Sorted because the modeller reads it so.
+        seq1_out = []
+        seq2_out = []
+        for chain in self.chains_ordered:
+            seq1_out.append("\n".join([self.structure_chains[chain]["sequence"][i:i+50] for i in range(0, len(self.structure_chains[chain]["sequence"]), 50)]))
+            seq2_out.append("\n".join([self.target_chains[chain]["sequence"][i:i+50] for i in range(0, len(self.target_chains[chain]["sequence"]), 50)]))
+        seq1_out = "\n".join([i+"/" if len(i) % 50 != 0 else i+"\n/" for i in seq1_out])
+        seq2_out = "\n".join([i+"/" if len(i) % 50 != 0 else i+"\n/" for i in seq2_out])
+
+        out = "\n".join([f">P1;{self.structure_name.upper()}",
+                         f"structure:{self.structure_name.upper()}:{self.structure_chains[self.chains_ordered[0]]['start']}:{self.chains_ordered[0]}:{self.structure_chains[self.chains_ordered[-1]]['end']}:{self.chains_ordered[-1]}:{self.structure_name.upper()}:::",
+                         seq1_out,
+                         "*",
+                         "",
+                         f">P1;{self.target_name}",
+                         f"sequence:{self.target_name}:1::::{self.target_name}:::",
+                         seq2_out,
+                         "*"])
+        with open(self.input_ali_loc, "w") as file:
+            file.write(out)
 
     def pdb_clean(self):
         """
@@ -200,8 +272,51 @@ class Input:
         """
         io = PDB.PDBIO()
         io.set_structure(self.structure)
-        selected_chains = _Select([x for x in self.structure[0].get_list() if x.id in self.structure_chains.keys()])
-        io.save(self.input_pdb_loc, selected_chains)
+        selected = _Select()
+        selected.extend([x for x in self.structure[0].get_list() if x.id in self.structure_chains.keys()], "c")
+
+        residues = {ch.id: list(ch.get_residues()) for ch in self.structure[0].get_list()
+                    if ch.id in self.structure_chains.keys()}
+        accepted_res = []
+        for chain, sequence in residues.items():
+            while SeqUtils.seq1("".join(aa.get_resname() for
+                                        aa in sequence[:5])) != self.structure_chains[chain]["sequence"][:5]:
+                sequence = sequence[1:]
+            while SeqUtils.seq1("".join(aa.get_resname() for
+                                        aa in sequence[-5:])) != self.structure_chains[chain]["sequence"][-5:]:
+                sequence = sequence[:-1]
+            accepted_res = accepted_res + sequence
+            residues[chain] = sequence
+        selected.extend(accepted_res, "r")
+
+        io.save(self.input_pdb_loc, selected)
+
+        # Adding the beginning and ending AAs to the list
+        for chain in self.structure_chains.keys():
+            self.structure_chains[chain]["start"] = residues[chain][0].full_id[-1][1]
+            self.structure_chains[chain]["end"] = residues[chain][-1].full_id[-1][1]
+
+
+class Model:
+    def __init__(self, settings, starting_model=5, ending_model=5):
+        """
+        The model creation object.
+        :param settings: Input object created for the input settings to the model.
+        """
+        self.env = modeller.environ()
+
+        os.chdir(settings.input_loc)
+        self.env.io.atom_files_directory = ["./"]
+
+        self.alignment = automodel.automodel(self.env,
+                                             alnfile=settings.input_ali_loc.split("/")[-1],
+                                             knowns=settings.structure_name.upper(),
+                                             sequence=settings.target_name)
+
+        self.alignment.starting_model = starting_model
+        self.alignment.ending_model = ending_model
+
+        self.alignment.make()
 
 
 if __name__ == "__main__":
@@ -218,17 +333,9 @@ if __name__ == "__main__":
     target = ("Alpha-6", "Beta-3")
     target_name = "a6b3"
 
-    input = Input(structure_loc,
-                  target,
-                  used_structure_name,
-                  target_name, pir_input)
+    model_input = Input(structure_loc,
+                        target,
+                        used_structure_name,
+                        target_name, pir_input)
 
-    # modeller.log.verbose()
-    # environment = modeller.environ()
-
-    # Database of sequence alignments reading/writing
-    # Modeller can create a binary DB, which works faster  than reading PIR file.
-    # We should opt for this once we create the alignment master-PIR.
-
-    # Alignment object creation
-    # alignment = modeller.alignment(environment)
+    model = Model(model_input)
